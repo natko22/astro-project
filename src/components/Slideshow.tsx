bd198979ca1f37b91c3823882560b78ac76ef0ec
Natassa <112552395+natko22@users.noreply.github.com>
2024-10-02 14:48:25 +0200

    centered slide add explore link

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -9,12 +9,12 @@ const ContinuousScrollingSlideshow: React.FC = () => {
     '/images/image4.svg',
   ];
 
-  // Duplicate  images array to create a seamless loop
-  const doubledImages = [...images,...images]; 
+  // Duplicate images array to create a seamless loop
+  const doubledImages = [...images, ...images]; 
 
   return (
-    <div className="relative w-full overflow-hidden h-60">
-      <div className="flex animate-marquee space-x-2 whitespace-no-wrap ">
+    <div className="relative w-full h-60 flex justify-center items-center overflow-hidden">
+      <div className="flex animate-marquee space-x-2 whitespace-no-wrap">
         {doubledImages.map((src, index) => (
           <div key={index} className="flex-shrink-0 w-full max-w-xs">
             <img src={src} alt={`Slide ${index}`} className="w-60 h-auto object-cover" />
@@ -24,6 +24,5 @@ const ContinuousScrollingSlideshow: React.FC = () => {
     </div>
   );
 };
-
 
 export default ContinuousScrollingSlideshow;
