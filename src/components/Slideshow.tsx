4e75de10b642ed04ebe4fd9e71ceeac5c9c4933c
Natassa <112552395+natko22@users.noreply.github.com>
2024-10-02 10:46:54 +0200

    create slideshow and animation

---
+++ src/components/Slideshow.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ContinuousScrollingSlideshow: React.FC = () => {
+
+  const images = [
+    '/images/image1.svg',
+    '/images/image2.svg',
+    '/images/image3.png',
+    '/images/image4.svg',
+  ];
+
+  // Duplicate  images array to create a seamless loop
+  const doubledImages = [...images,...images]; 
+
+  return (
+    <div className="relative w-full overflow-hidden mt-20">
+      <div className="flex animate-marquee space-x-2 whitespace-no-wrap ">
+        {doubledImages.map((src, index) => (
+          <div key={index} className="flex-shrink-0 w-full max-w-xs">
+            <img src={src} alt={`Slide ${index}`} className="w-60 h-auto object-cover" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default ContinuousScrollingSlideshow;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				marquee: {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(-100%)' },
+				},
+			},
+			animation: {
+				marquee: 'marquee 18s linear infinite',
+
+			},
+		},
 	},
 	plugins: [],
-}
+};
