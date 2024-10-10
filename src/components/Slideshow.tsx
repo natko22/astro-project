c63196b0572851cc6c3439c7e5f80b22edc1ba35
Natassa <112552395+natko22@users.noreply.github.com>
2024-10-10 11:00:53 +0200

    install react fast marque ,adjust size image

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React from "react";
+import Marquee from "react-fast-marquee"; // Importing react-fast-marquee
 
 const ContinuousScrollingSlideshow: React.FC = () => {
-
   const images = [
-    '/images/slide/image1.svg',
-    '/images/slide/image2.svg',
-    '/images/slide/image3.png',
-    '/images/slide/image4.svg',
+    "/images/slide/image1.svg",
+    "/images/slide/image2.svg",
+    "/images/slide/image3.png",
+    "/images/slide/image4.svg",
   ];
 
-  // Duplicate images array to create a seamless loop
-  const doubledImages = [...images, ...images]; 
-
   return (
-    <div className="relative w-full h-60 flex justify-center items-center overflow-hidden">
-      <div className="flex animate-marquee space-x-2 whitespace-no-wrap">
-        {doubledImages.map((src, index) => (
+    <div className="relative w-full h-60 flex justify-center items-center overflow-hidden ">
+      <Marquee
+        speed={60}
+        gradient={false}
+        pauseOnHover={true}
+        loop={0}
+        className="space-x-2"
+      >
+        {images.map((src, index) => (
           <div key={index} className="flex-shrink-0 w-full max-w-xs">
-            <img src={src} alt={`Slide ${index}`} className="w-60 h-auto object-cover" />
+            <img
+              src={src}
+              alt={`Slide ${index}`}
+              className="w-60 h-20 object-contain"
+            />
           </div>
         ))}
-      </div>
+      </Marquee>
     </div>
   );
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,17 @@
-
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			keyframes: {
-				marquee: {
-					'0%': { transform: 'translateX(0)' },
-					'100%': { transform: 'translateX(-100%)' },
-				},
-			},
-			animation: {
-				marquee: 'marquee 18s linear infinite',
-
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'), 
-
-	],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 18s linear infinite",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
